26a05265460773a20c916054a3b2fb9bdbe2acb0
Brun04 <bruno.verchere04@orange.fr>
2020-12-26 21:29:23 +0100

    Multiple geocoders

--- src/csvgeocoder.js
+++ src/csvgeocoder.js
@@ -1,31 +1,58 @@
-window.onload = init;var map;var markers=undefined;var tabData=undefined;var gridOptions=undefined;selectedCol = {'geocode':[], 'content':[]};
-
-
+window.onload = init;var map; var markers=undefined;
+var tabData=undefined; var gridOptions=undefined; var selectedCol = {'geocoding':[], 'content':[]};
+var geocoders = undefined;
 
 
 function init(){
-	document.querySelector('#geodownload').disabled = true;
-    document.getElementById("upload-file").addEventListener("click", function(e){
-        e.preventDefault();
-        Papa.parse(document.querySelector("#files").files[0], {
-            delimiter: ";",header: true,
-            error: function(err, file){console.error("ERROR:", err, file)},
-            complete: function(results){displayHTMLTable(results);console.log("Done with all files")}
-        });
-    })
+	// Files
+	files = document.getElementById('files');
+	document.querySelector('#geoDownloader').disabled = true;
+    document.getElementById("fileUploader").addEventListener("click", function(){
+		if(files){ files.click(); }
+	}, false);
+	files.addEventListener('change', handleFiles, false);
+	// Map
     map = L.map('map').setView([45, 3], 5);markers = L.featureGroup([]);
     L.tileLayer('https://{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png', {attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'}).addTo(map);
-    document.getElementById("display-map").addEventListener("click", displayMap);
+    document.getElementById("mapDisplayer").addEventListener("click", displayMap);
 	displayMap(undefined);
+	// Set geocoders
+	fetch('geocoders.json')
+		.then(r=>r.json())
+		.then(r=>{
+			geocoders = r;
+			var geocodersInput = document.querySelector('#geocodersInput');
+			for(elem in geocoders){
+				if(!geocoders[elem].url.includes('YOUR_ACCESS_KEY')){
+					let htmlElements = createGeoRadioInput(elem, geocoders[elem].name);
+					geocodersInput.append(htmlElements[0], htmlElements[1]);
+				}
+			  }
+		})
+}
 
-	document.querySelector('#burger-open').addEventListener("click", function(){
-		document.querySelector('#burger-open').classList.remove("is-active");
-		document.querySelector('#burger-close').classList.add("is-active");
-	});
-	document.querySelector('#burger-close').addEventListener("click", function(){
-		document.querySelector('#burger-close').classList.remove("is-active");
-		document.querySelector('#burger-open').classList.add("is-active");
-	});
+function createGeoRadioInput(shortName, name){
+	var input = document.createElement('input');
+	input.type = 'radio';
+	input.name = 'geocoder';
+	input.value = shortName;
+	var label = document.createElement('label');
+	label.for = shortName;
+	label.innerText = name;
+	return [input, label];
+}
+
+function handleFiles(){
+	if(!this.files.length){
+		alert('No files given');
+	}else{
+		selectedCol = {'geocoding':[], 'content':[]};
+		Papa.parse(document.querySelector("#files").files[0], {
+			delimiter: ";", header: true,
+			error: function(err, file){ console.error("ERROR:", err, file); },
+			complete: function(results){ displayHTMLTable(results); }
+		});
+	}
 }
 
 function newRow(tabHeader){
@@ -34,38 +61,56 @@ function newRow(tabHeader){
     return row;
 }
 
+function getGeocoder(){
+	var geocodersInput = document.querySelectorAll('input[name="geocoder"]');
+	for(let i=0; i < geocodersInput.length; i++){
+		if(geocodersInput[i].checked){return geocoders[geocodersInput[i].value];}
+	}
+	return geocoders['ban'];
+}
+
 function displayHTMLTable(results){
     tabData = results.data;
-    var tabHeader = [{headerName: "N°", field: "nm", filter: "agNumberColumnFilter"}];
-    results.meta.fields.forEach(field => {
-        tabHeader.push({headerName: field, field: field, filter: "agTextColumnFilter"});
-    });
-    var extendedHeader = [
-        {headerName:"Longitude", field:"lng", filter: "agNumberColumnFilter"},
-        {headerName:"Latitude", field:"lat", filter: "agNumberColumnFilter"},
-        {headerName:"Label", field:"label", filter: "agTextColumnFilter"},
-        {headerName:"Score", field:"score", filter: "agNumberColumnFilter"}
-    ];
-    tabHeader = tabHeader.concat(extendedHeader);
-    var rowData= [];
-    gridOptions = {
-        defaultColDef: {
-            sortable: true,
-            resizable: true,
-        },
-        columnDefs: tabHeader,
-        rowData: rowData
-    };
-
 	setTabOptions();
-	document.querySelector('#tab-opt-popup').classList.add('is-displayed');
-	document.querySelector('#close-popup-opt').addEventListener('click', closeOptPopup);
-	document.querySelector('#geodownload').addEventListener('click', exportDataAsCsv);
-	
-	document.querySelector('#btn-geocode').addEventListener('click', function(){
-		document.querySelector("#grid-tab").innerHTML = '';
-		new agGrid.Grid(document.querySelector("#grid-tab"), gridOptions);
+	document.querySelector('input[value="ban"]').checked = true;
+	document.querySelector('#optionsPopup').classList.add('is-active');
+	document.querySelector('#optionsClose').addEventListener('click', closeOptPopup);
+	document.querySelector('#geocoderBtn').addEventListener('click', function(){
+		// Get selected geocoder
+		var geocoderData = getGeocoder();
+		// Init grid
+		var tabHeader = [{headerName: "N°", field: "nm", filter: "agNumberColumnFilter"}];
+		results.meta.fields.forEach(field => {
+			tabHeader.push({headerName: field, field: field, filter: "agTextColumnFilter"});
+		});
+		var extendedHeader = [
+			{headerName:"Longitude", field:"lng", filter: "agNumberColumnFilter"},
+			{headerName:"Latitude", field:"lat", filter: "agNumberColumnFilter"},
+			{headerName:"Label", field:"label", filter: "agTextColumnFilter"}
+			
+		];
+		if(geocoderData.schema.score !== null){
+			extendedHeader.push({headerName:"Score", field:"score", filter: "agNumberColumnFilter"});
+		}
+		tabHeader = tabHeader.concat(extendedHeader);
+		var rowData= [];
+		gridOptions = {
+			defaultColDef: {
+				sortable: true,
+				resizable: true,
+			},
+			columnDefs: tabHeader,
+			rowData: rowData
+		};
+		document.querySelector("#gridTab").innerHTML = '';
+		new agGrid.Grid(document.querySelector("#gridTab"), gridOptions);
+		document.querySelector('#gridTab').classList.add('is-active');
+		// Init HTML related elements
+		document.querySelector('#geoDownloader').addEventListener('click', exportDataAsCsv);
+		document.querySelector('#geoDownloader').classList.remove('is-disable');
+		// Clear map
 		markers.clearLayers();
+		// Fill the table
 		for(i=0;i<tabData.length-1;i++){
 			var cells = tabData[i]
 			var row = newRow(tabHeader);
@@ -74,14 +119,16 @@ function displayHTMLTable(results){
 			});
 			row['nm'] = i+1;
 			rowData.push(row);
-			geocoder(cells, row, gridOptions);
-		} 
+			geocode(cells, row, gridOptions, geocoderData);
+		}
 	});
+
+	
 }
 
 function closeOptPopup(){
-	document.querySelector('#tab-opt-popup').classList.remove('is-displayed');
-	document.querySelector('#close-popup-opt').removeEventListener('click', closeOptPopup);
+	document.querySelector('#optionsPopup').classList.remove('is-active');
+	document.querySelector('#optionsClose').removeEventListener('click', closeOptPopup);
 }
 
 function newCheckbox(value, i, type){
@@ -101,7 +148,7 @@ function newOptRow(key, i){
 	r.appendChild(tdLabel);
 	// Geocoding
 	let tdG = document.createElement('td');
-	tdG.appendChild(newCheckbox(key, i, 'geocode'));
+	tdG.appendChild(newCheckbox(key, i, 'geocoding'));
 	r.appendChild(tdG);
 	// Content
 	let tdC = document.createElement('td');
@@ -112,62 +159,76 @@ function newOptRow(key, i){
 
 function setTabOptions(){
 	if(typeof tabData != 'undefined'){
-		var optTable = document.querySelector('#geocoder-options');
+		var optTable = document.querySelector('#geocodingOptions');
 		optTable.innerHTML = '';
 		var i = 1;
 		Object.keys(tabData[0]).forEach(key=>{
 			optTable.appendChild(newOptRow(key, i));i++;
         });
-		document.querySelector('#tab-opt-popup').style.height=80+(50*Object.keys(tabData[0]).length)+'px';
+		document.querySelector('#optionsPopup').style.height = 100 + (50*Object.keys(tabData[0]).length)+'px';
 	}
 }
 
 function checkboxChange(target){
 	if(selectedCol[target.name.split('check')[0]].includes(target.value)){
-		//target.checked = false;
 		const idx = selectedCol[target.name.split('check')[0]].indexOf(target.value);
 		if(idx > -1){selectedCol[target.name.split('check')[0]].splice(idx, 1);}
 	}else{
-		//target.checked = true;
 		selectedCol[target.name.split('check')[0]].push(target.value);
 	}
 }
 
-function geocoder(cells, row,  gridOptions){
-	var adress = "";
-	if(typeof tabData != 'undefined' && selectedCol['geocode'].length > 0){
-		for(j=0;j<selectedCol['geocode'].length;j++){
-			if(cells[selectedCol['geocode'][j]] != ''){adress+=cells[selectedCol['geocode'][j]]+" ";}
+function getJSONValue(obj, idx, keys){
+  if(idx == keys.length-1){ return obj[keys[idx]]; }
+  return getJSONValue(obj[keys[idx]], idx+1, keys);
+}
+
+function geocode(cells, row, gridOptions, geocoderData){
+	var adress = ""; var schema = geocoderData.schema;
+	if(typeof tabData != 'undefined' && selectedCol['geocoding'].length > 0){
+		for(j=0;j<selectedCol['geocoding'].length;j++){
+			if(cells[selectedCol['geocoding'][j]] != ''){adress+=cells[selectedCol['geocoding'][j]]+" ";}
 		}
-		fetch('http://api-adresse.data.gouv.fr/search/?q='+adress.slice(0, adress.length-1)).then(r=>r.json())
+		fetch(geocoderData.url+adress.slice(0, adress.length-1))
+		.then(r=>r.json())
 		.then(r=>{
-			var imax=0;var scoremax=0;
-			if(r["features"].length > 0){
-				for(let k=0; k < r["features"].length; k++){
-					if(r["features"][k]["properties"]["score"] > scoremax){scoremax = r["features"][k]["properties"]["score"];imax = k;}
+			var imax=0;var scoremax=0;var feature;
+			if(r[schema.results].length > 0){
+				// Get the feature
+				if(geocoderData.schema.score !== null){
+					for(let k=0; k < r[schema.results].length; k++){
+						var current = getJSONValue(r[schema.results][k], 0, schema.score);
+						if(current > scoremax){ scoremax = current; imax = k;}
+					}	
 				}
-				var bestFeat = r["features"][imax];
-				row['lng']= bestFeat['geometry']['coordinates'][0];
-				row['lat']= bestFeat['geometry']['coordinates'][1];
-				row['label']=bestFeat['properties']['label'];
-				row['score']= bestFeat['properties']['score'].toFixed(3);
+				feature = r[schema.results][imax];
+				// Process with the feature
+				row['lng']= getJSONValue(feature, 0, schema.lng);
+				row['lat']= getJSONValue(feature, 0, schema.lat);
+				row['label']= getJSONValue(feature, 0, schema.label);
+				if(geocoderData.schema.score !== null){
+					row['score'] = getJSONValue(feature, 0, schema.score).toFixed(3);
+				}
+				// Set marker content
 				var content = "";
 				for(k=0;k<selectedCol['content'].length;k++){
 					content+="<p><strong>"+selectedCol['content'][k]+"</strong>: "+cells[selectedCol['content'][k]]+"</p>";
 				}
+				console.log(content);
 				const marker = L.marker([row['lat'], row['lng']], {title: cells[selectedCol['content'][0]]}).bindPopup(content)
 				markers.addLayer(marker);markers.addTo(map);
+				// Add to the table
 				gridOptions.api.applyTransaction({add: [row]});
-			}
-			else{
-				//console.warn("No result found with "+tabData[idx]);
+			}else{
+				console.warn("No result found with "+tabData[idx]);
 				row['lng']= null;
 				row['lat']= null;
 				row['label']='';
 				row['score']=0;	
 			}
 			closeOptPopup();
-			document.querySelector('#geodownload').disabled = false;
+			document.querySelector('#geoDownloader').disabled = false;
+			
 		})
 	}
 }
@@ -175,17 +236,17 @@ function geocoder(cells, row,  gridOptions){
 function displayMap(event){
 	if(typeof event != 'undefined'){event.preventDefault();}
 	let mapDiv = document.querySelector('#map-div');
-	if(!mapDiv.classList.contains('is-displayed')){mapDiv.classList.add("is-displayed");}
-	document.getElementById("display-map").innerHTML = '<img src="assets/targetWhite.svg"> Center map';
+	if(!mapDiv.classList.contains('is-active')){mapDiv.classList.add("is-active");}
+	document.getElementById("mapDisplayer").innerHTML = '<img src="assets/targetWhite.svg"> Center map';
 	if(markers.getLayers().length>0){map.fitBounds(markers.getBounds());}
 	else{map.setView(new L.latLng(45, 3), 5);}
     document.getElementById("hide-map").addEventListener("click", hideMap);
 }
 
 function hideMap(){
-    document.getElementById("map-div").classList.remove("is-displayed");
+    document.getElementById("map-div").classList.remove("is-active");
     document.getElementById("hide-map").removeEventListener("click", hideMap);
-	document.getElementById("display-map").innerHTML = '<img src="assets/map.svg"> Display map';
+	document.getElementById("mapDisplayer").innerHTML = '<img src="assets/map.svg"> Display map';
 }
 
 function exportDataAsCsv(e){
